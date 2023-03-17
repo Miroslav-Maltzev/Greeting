5dcc26410558071099fa5c6ad9a1d4806ff2469c
Miroslav-Maltzev <125460844+Miroslav-Maltzev@users.noreply.github.com>
2023-03-17 18:07:29 +0300

    Deleted file Changes.txt and edited file Greeting.cs

--- greeting.cs
+++ greeting.cs
@@ -2,54 +2,37 @@ using System;
 
 namespace Greeting
 {
-    public class greeting
+    public class Greeting
     {
         public static void Main(string[] args)
         {
             Console.Write("Hello! What is your name?");
             string name = Console.ReadLine();
-            string time = Convert.ToString(DateTime.Now);
-            int mins = Convert.ToInt32(time.Substring(14, 2));
-            int hours = Convert.ToInt32(time.Substring(11, 2)) + 3; //+3 because of belarus timezone
-            Console.Write(Answer(hours, mins, name)); //If you want you can change variables "hours" and "mins" to any numbers to debug
-            
+            int hours = DateTime.Now.Hour + 3;
+            Console.Write(Answer(hours, name)); //If you want you can change variable "hours" to any number to debug
         }
         
-        public static string Answer(int hours, int mins, string name)
+        public static string Answer(int hours, string name)
         {
             string appeal = "";
-            if (IsDateBetween(hours, mins, 0, 0, 12, 0))
+            if (hours >= 0 && hours < 12)
             {
                 appeal = "Good morning";
             }
-            else if (IsDateBetween(hours, mins, 12, 0, 15, 0))
+            else if (hours >= 12 && hours < 15)
             {
                 appeal = "Good day";
             }
-            else if (IsDateBetween(hours, mins, 15, 0, 22, 0))
+            else if (hours >= 15 && hours < 22)
             {
                 appeal = "Good evening";
             }
-            else if (IsDateBetween(hours, mins, 22, 0, 24, 0))
+            else if (hours >= 22 && hours < 24)
             {
                 appeal = "Good night";
             }
             appeal = appeal + $", {name}!";
             return appeal;
-        }
-        
-        public static bool IsDateBetween(int dateHours, int dateMins, int date1Hours, int date1Mins, int date2Hours, int date2Mins)
-        {
-            int date = dateMins + dateHours*60;
-            int date1 = date1Mins + date1Hours*60;
-            int date2 = date2Mins + date2Hours*60;
-            
-            bool isBetween = false;
-            if (date2 >= date && date >= date1)
-            {
-                isBetween = true;
-            }
-            return isBetween;
         }
     }
 }
